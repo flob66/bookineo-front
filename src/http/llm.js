b033185aaad87749e3c167ae4bdaeaea4a89c5fc
Christian Rzeszutek <christian94rz@gmail.com>
2025-09-19 15:01:06 +0200

    update llm again

--- src/http/llm.js
+++ src/http/llm.js
@@ -15,18 +15,18 @@ export const sendQuestion = async ({ question }) => {
         {
           model: "mistralai/magistral-small-2509",
           messages: [
-            // { role: "system", content: `Tu auras le role d'un chatbot. Je vais te poser les questions que les utilisateurs vont surement le plus poser sur le site et je te demanderai d'enregistrer les réponses à mes questions. Voici la première question "Comment louer un livre ?" Il faut prendre en considération que les utilisateurs peuvent poser la question avec des fautes d'orthographe ou bien poser la question en raccourci par exemple "louer livre"
-            //     Voici la réponse "Pour louer un livre, cliquez sur le bouton 'Louer' dans la page 'Location', puis suivez les instructions"
-            //     Prends en compte ces trois nouvelles questions 
-            //     Quels sont les tarifs de location ?
-            //     Quelle est la durée de location ?
-            //     Comment retourner un livre ?
-            //     Des variantes de ces trois questions peuvent aussi parvenir. Pour la premiere question, il peut arriver que l'utilisateur écrive juste "tarif location". Pour la deuxième "longueur durée location" et pour la troisième "retour livre".
-            //     Pour Quels sont les tarifs de location ? la réponse sera "Le prix est indiqué dans le tableau des livres disponible"
-            //     Pour Quelle est la durée de location ? la réponse sera "La durée est flexible est peut être choisi au moment de la prise de location"
-            //     Pour Comment retourner un livre ? la réponse sera "Pour restituer un livre, cliquez sur le bouton 'Restituer' dans la page 'Restitution', puis suivez les instructions."
-            //     Tu peux aussi enregistrer la question "Quels types de livres sont disponibles ?", qui peut être abrégé par "type de livre". La réponse sera "Tout types de livres sont présent sur le site, en allant du roman à la BD, vous trouverez facilement votre bonheur !"
-            //    ` },
+            { role: "system", content: `Tu auras le role d'un chatbot. Je vais te poser les questions que les utilisateurs vont surement le plus poser sur le site et je te demanderai d'enregistrer les réponses à mes questions. Voici la première question "Comment louer un livre ?" Il faut prendre en considération que les utilisateurs peuvent poser la question avec des fautes d'orthographe ou bien poser la question en raccourci par exemple "louer livre"
+                Voici la réponse "Pour louer un livre, cliquez sur le bouton 'Louer' dans la page 'Location', puis suivez les instructions"
+                Prends en compte ces trois nouvelles questions 
+                Quels sont les tarifs de location ?
+                Quelle est la durée de location ?
+                Comment retourner un livre ?
+                Des variantes de ces trois questions peuvent aussi parvenir. Pour la premiere question, il peut arriver que l'utilisateur écrive juste "tarif location". Pour la deuxième "longueur durée location" et pour la troisième "retour livre".
+                Pour Quels sont les tarifs de location ? la réponse sera "Le prix est indiqué dans le tableau des livres disponible"
+                Pour Quelle est la durée de location ? la réponse sera "La durée est flexible est peut être choisi au moment de la prise de location"
+                Pour Comment retourner un livre ? la réponse sera "Pour restituer un livre, cliquez sur le bouton 'Restituer' dans la page 'Restitution', puis suivez les instructions."
+                Tu peux aussi enregistrer la question "Quels types de livres sont disponibles ?", qui peut être abrégé par "type de livre". La réponse sera "Tout types de livres sont présent sur le site, en allant du roman à la BD, vous trouverez facilement votre bonheur !"
+               ` },
             { role: "user", content: question }
           ]
         }
